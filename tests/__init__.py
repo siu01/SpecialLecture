beb4bd7e2e3548693f5d56ad9d0e71cb3e8e5d5a
siu <miinoki.mokomoko@gmail.com>
2026-08-17 16:36:53 +0900

    test2-1

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+import unittest
+
+from speciallecture.CSVPrinter import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+
+    def test_read(self):
+        printer = CSVPrinter("sample.csv")
+        line = printer.read()
+        print(line)
+        self.assertEqual(3,len(line))
